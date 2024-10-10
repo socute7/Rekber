2b919a83181e5886785c78b5fe6751d1f2de538a
Aji Darma S <73057336+socute7@users.noreply.github.com>
2024-10-10 18:38:34 +0700

    rekber

---
+++ app/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+import Login from './../components/Login'
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+    >
+      <Login/>
+    </View>
+  );
+}
